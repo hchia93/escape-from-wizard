65a46b79ee539baf9f364249fd704d660766d0e5
Hyrex Chia JianFei <10288953+hchia93@users.noreply.github.com>
2025-06-03 17:47:28 +0800

    Project setup

---
+++ EscapeFromWizard/src/Audio/SoundManager.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+namespace DungeonRE.Audio
+{
+    public class SoundManager
+    {
+        public SoundEffect buttonSound;
+        public SoundEffect gameOverSound;
+        public SoundEffect getItemSound;
+        public SoundEffect footstepSound;
+        public SoundEffect unlockDoorSound;
+        public SoundEffect hitByMinionSound;
+        public SoundEffect hitByWizardSound;
+        public SoundEffect recoverHPSound;
+        public SoundEffect BGMSound;
+        public SoundEffect winningSound;
+        public SoundEffectInstance BGMInstance;
+        public SoundEffectInstance GameOverBGMInstance;
+
+        public SoundManager(ContentManager Content)
+        {
+            buttonSound = Content.Load<SoundEffect>(@"Resource\Audio\ButtonSFX");
+            gameOverSound = Content.Load<SoundEffect>(@"Resource\Audio\GameOverSFX");
+            getItemSound = Content.Load<SoundEffect>(@"Resource\Audio\GetItemSFX");
+            footstepSound = Content.Load<SoundEffect>(@"Resource\Audio\FootstepSFX");
+            BGMSound = Content.Load<SoundEffect>(@"Resource\Audio\Tombi_Dwarf_Forest_BGM");
+            unlockDoorSound = Content.Load<SoundEffect>(@"Resource\Audio\UnlockDoorSFX");
+            winningSound = Content.Load<SoundEffect>(@"Resource\Audio\Winning");
+            hitByWizardSound = Content.Load<SoundEffect>(@"Resource\Audio\HitByWizardSFX");
+            hitByMinionSound = Content.Load<SoundEffect>(@"Resource\Audio\HitByMinionSFX");
+            recoverHPSound = Content.Load<SoundEffect>(@"Resource\Audio\HPRecoverySFX");
+
+        }
+
+        public void PlayBGM()
+        {
+          
+            BGMInstance = BGMSound.CreateInstance();
+            BGMInstance.IsLooped = true;
+            BGMInstance.Play();
+                   
+        }
+
+        public void PlayFootstepSound()
+        {
+            footstepSound.Play(0.15f, 0.0f, 0.0f);
+        }
+
+        public void PlayUnlockDoorSound()
+        {
+            unlockDoorSound.Play(0.15f, 0.0f, 0.0f);
+        }
+
+        public void PlayHidingSound()
+        {
+            
+           buttonSound.Play(0.30f, 0.0f, 0.0f);
+               
+        }
+
+        public void PlayPickUpSound()
+        {
+           getItemSound.Play();
+        }
+
+        public void PlayHitByMinionSound()
+        {
+            hitByMinionSound.Play();
+        }
+
+        public void PlayHitByWizardSound()
+        {
+            hitByWizardSound.Play();
+        }
+
+        public void PlayRecoverHPSound()
+        {
+            recoverHPSound.Play();
+        }
+
+        public void PlayWinningSound()
+        {
+            winningSound.Play();
+        }
+
+        public void PlayGameOverSound()
+        {
+            BGMInstance.Stop();
+            GameOverBGMInstance = gameOverSound.CreateInstance();
+            GameOverBGMInstance.IsLooped = true;
+            GameOverBGMInstance.Play();
+        }
+
+        public void StopGameOverSound()
+        {
+            GameOverBGMInstance.Stop();
+        }
+
+        public void StopBGM()
+        {
+            BGMInstance.Stop();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# escape-from-wizard
+# Escape From Wizard
+Escape from Wizard is a C# project I've made during University Course ` Game Algorithm Design and Analysis` using ![Microsoft XNA 4.0](https://en.wikipedia.org/wiki/Microsoft_XNA_Game_Studio#:~:text=Microsoft%20XNA%20Game%20Studio%20is,on%20the%20Microsoft%20XNA%20platform.) framework. It is featureing A* algorithm implementation for the path searching.
 
+It was initially using Microsoft XNA framework which tied to VS2015. It is now upgraded to VS2022 with ![MonoGame](https://monogame.net/) and now it is runnable.
+
+<center>
+    <img src="escape-from-wizard.png" width="250">
+</center>
+
+## Setting Up
+Unfortunately, this is not the kind of project can be download and run directly. I have excluded most of the buildable binaries files from this project as best practices. One must setup the environment properly to run this project.
+
+Please go thru the following setup documentation:
+
+![Setting up your OS for development](https://docs.monogame.net/articles/getting_started/1_setting_up_your_os_for_development_windows.html?tabs=android)
+
+![Setting up Visual Studio on Windows](https://docs.monogame.net/articles/getting_started/2_choosing_your_ide_visual_studio.html)
+
+## Compiling Content 
+After upgrade from Microsoft XNA 4.0, the old method of loading resources is no longer working. Hence, you must link the resources using MGCB and build them on your machine. 
+
+Refer to: 
+![Generating XNB files](https://www.trccompsci.online/mediawiki/index.php/Generating_and_using_XNB_files)
+
+<center>
+<div class="row">
+    <div class="column"></div>
+        <img src="open-with-mgcb.png" width="250">
+        <img src="build-with-mgcb.png" width="250">
+    <div class="column"></div>
+        
+</div>
+</center>
+
+Once `\bin` and `\obj` should be generated in `...\EscapeFromWizard\Content`
+
+<img src="generated-dir-mgcb.png" width="100">
+
+The project is now good to go.
+
+## How to play
+`←` `→` `↑` `↓` to move around.
+
+`!` are hiding tiles. Wizard will lose sight of you after you entering the location. Will pushes you back out after a short period of time.
+
+`Star` are related to scores.
+
+`Wizard` and `Ghost` do damage on contact. When you lose all health, you lose.
+
+`Purple Potion` are quest items. You must collect all 3 quest item before reaching the `Exit Sign`.
+
+`Red Potion` are health potions.
+
+Collect `Color Key` to unlock `Color Door`.
+
+`Esc` to quit the game.
+
+`F1` for debug index.
+
+Reach `Exit Sign` to win the game.
